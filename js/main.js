c7a64db01b81ab9de44166d1aa72e029949a97ec
Deborah W - laptop <deborah.0214@hotmail.com>
2023-03-04 21:16:13 -0800

    js code works for toggle

--- js/main.js
+++ js/main.js
@@ -1,37 +1,28 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    if(slideIndex === 0){document.getElementsByClassName("carousel-item")[0].style.display = "block"}
-    let slides = document.getElementsByClassName("carousel-item");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-'use strict';
-
-// FSM VARIABLES
-let state = 0;
+// let slideIndex = 0;
+// showSlides();
+//
+// function showSlides() {
+//     let i;
+//     if(slideIndex === 0){document.getElementsByClassName("carousel-item")[0].style.display = "block"}
+//     let slides = document.getElementsByClassName("carousel-item");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+// 'use strict';
+//
+// // FSM VARIABLES
+// let state = 0;
 
 // MENU THINGS
 function menuToggleNew() {
-    let menuBtn = document.querySelector('#menuBtn');
-    menuBtn.classList.toggle("hidden");
-    menuLookToggle(menuBtn);
-}
-
-function menuLookToggle(x) {
-    x.classList.toggle("change");
-    if (state === 0) {
-        state = 1;
-    } else {
-        state = 0;
-    }
+    let menuOverlay = document.querySelector('#menuOverlay');
+    let displayState = menuOverlay.classList.toggle('hidden');
+    console.log(displayState);
 }
 
 
